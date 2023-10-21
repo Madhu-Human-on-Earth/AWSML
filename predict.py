0a04ee95980cd476fcc0bf8632ae30a6b319a04a
MADHU <116476425+Madhu-Human-on-Earth@users.noreply.github.com>
2023-10-22 00:17:23 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,58 +1,84 @@
-import numpy as np
-import torch
-from torchvision import datasets, transforms
-from torch import nn
-from torch import optim
-import torch.nn.functional as F
-import torchvision.models as models
-from collections import OrderedDict
-from PIL import Image
-import glob, os
-import utilize
-import model_functions as fm
 import argparse
+import json
+import torch
+import numpy as np
+from math import ceil
+from PIL import Image
+from train import check_gpu
+from torchvision import models
 
-def predicts(args):
-    import json
-    print("loading the checkpoint")
-    model=fm.loadCheckpoint(args.checkpoint_path)
-    print("Loading is finished")
+def arg_parser():
+    parser = argparse.ArgumentParser(description="predict.py")
+    parser.add_argument('--image', type=str, help='Path to image file for prediction.', required=True)
+    parser.add_argument('--checkpoint', type=str, help='Path to checkpoint file as str.', required=True)
+    parser.add_argument('--top_k', type=int, help='Choose top K matches as int.')
+    parser.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
+    parser.add_argument('--gpu', dest="gpu", action="store_true", default=False)
 
-    device="cpu"
-    if(args.gpu==True):
-        device=utilize.ProcessType()
-    model.to(device);
-    indexClass={}
-    for i,value in model.class_to_idx.items():
-        indexClass[value]=i
-    probs, classes = utilize.predict(args.input, model,args.top_k,device,indexClass)
-    print("Most "+str(args.top_k)+" Classes with their probabilities" )
-    if(args.category_names):
-        flower_to_name=[]
-        with open(args.category_names, 'r') as f:
-            flower_to_name = json.load(f)
-        classes_label=[]
-        print(probs.shape)
-        for i in classes:
-            classes_label.append(flower_to_name.get(str(i)))
-        for i in range(args.top_k):
-            print ("Class name: ",classes_label[i],",Probability :" ,probs[0][i].detach().numpy()*100)
-    else:
-        for i in range(args.top_k):
-            print("Class #", classes[i],"Probability: ",probs[0][i].detach().numpy()*100)
+    args = parser.parse_args()
+    return args
 
+def load_checkpoint(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    model = models.vgg16(pretrained=True)
+    model.class_to_idx = checkpoint['class_to_idx']
+    model.classifier = checkpoint['classifier']
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
+
+def process_image(image_path):
+    img = Image.open(image_path)
+    size = 256, 256
+    img.thumbnail(size)
+    width, height = img.size
+    crop_size = 244
+    left = (width - crop_size) / 2
+    top = (height - crop_size) / 2
+    right = (width + crop_size) / 2
+    bottom = (height + crop_size) / 2
+    img = img.crop((left, top, right, bottom))
+    np_image = np.array(img) / 255.0
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - mean) / std
+    np_image = np_image.transpose((2, 0, 1))
+    return np_image
+
+def predict(image_tensor, model, device, cat_to_name, topk=5):
+    model.eval()
+    with torch.no_grad():
+        image_tensor = torch.from_numpy(image_tensor).float()
+        image_tensor = image_tensor.unsqueeze(0)
+        image_tensor = image_tensor.to(device)
+        output = model(image_tensor)
+        ps = torch.exp(output)
+        top_probs, top_indices = ps.topk(topk, dim=1)
+        top_probs = top_probs.cpu().numpy().tolist()[0]
+        top_indices = top_indices.cpu().numpy().tolist()[0]
+        idx_to_class = {val: key for key, val in model.class_to_idx.items()}
+        top_labels = [idx_to_class[idx] for idx in top_indices]
+        top_flowers = [cat_to_name[label] for label in top_labels]
+        return top_probs, top_labels, top_flowers
+
+def print_probability(probs, flowers):
+    for i, (flower, prob) in enumerate(zip(flowers, probs), 1):
+        print(f"Rank {i}: Flower: {flower}, Likelihood: {ceil(prob * 100)}%")
 
 def main():
-    parser = argparse.ArgumentParser(description='Flowers Classifcation Predictor')
-    parser.add_argument('--input', type=str, help='path for image to predict',required=True)
-    parser.add_argument('--checkpoint_path', type=str, default='checkpoint.pth', help='path to en existance  checkpoint',required=True)
-    parser.add_argument('--top_k', type=int, default=5, help='top k classes for the input')
-    parser.add_argument('--category_names', type=str, help='json path file of categories names of flowers')
-    parser.add_argument('--gpu' , type=bool, default=False, help='checkpoint directory path')
-    args = parser.parse_args()
-    print(args)
+    args = arg_parser()
     
-    predicts(args)
-    print("\nPrediction is finished\n")
-if __name__ == "__main__":
+    with open(args.category_names, 'r') as f:
+        cat_to_name = json.load(f)
+    
+    model = load_checkpoint(args.checkpoint)
+    
+    image_tensor = process_image(args.image)
+    
+    device = check_gpu(args.gpu)
+    
+    top_probs, top_labels, top_flowers = predict(image_tensor, model, device, cat_to_name, args.top_k)
+    
+    print_probability(top_probs, top_flowers)
+
+if __name__ == '__main__':
     main()
