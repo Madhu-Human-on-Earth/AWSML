5b50889c41e4c677fcd7d5a51b3deab04d3b65f6
MADHU <116476425+Madhu-Human-on-Earth@users.noreply.github.com>
2023-10-22 00:12:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,58 +1,171 @@
-import numpy as np
+import argparse
 import torch
-from torchvision import datasets, transforms
+from collections import OrderedDict
+from os.path import isdir
 from torch import nn
 from torch import optim
-import torch.nn.functional as F
-import torchvision.models as models
-from collections import OrderedDict
-from PIL import Image
-import glob, os
-import utilize
-import model_functions as fm
-import argparse
+from torchvision import datasets, transforms, models
 
-def predicts(args):
-    import json
-    print("loading the checkpoint")
-    model=fm.loadCheckpoint(args.checkpoint_path)
-    print("Loading is finished")
+def arg_parser():
+    parser = argparse.ArgumentParser(description="Train.py")
+    parser.add_argument('--arch', dest="arch", action="store", default="vgg16", type=str)
+    parser.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
+    parser.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001, type=float)
+    parser.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
+    parser.add_argument('--epochs', dest="epochs", action="store", type=int, default=1)
+    parser.add_argument('--gpu', dest="gpu", action="store", default="gpu")
+    args = parser.parse_args()
+    return args
 
-    device="cpu"
-    if(args.gpu==True):
-        device=utilize.ProcessType()
-    model.to(device);
-    indexClass={}
-    for i,value in model.class_to_idx.items():
-        indexClass[value]=i
-    probs, classes = utilize.predict(args.input, model,args.top_k,device,indexClass)
-    print("Most "+str(args.top_k)+" Classes with their probabilities" )
-    if(args.category_names):
-        flower_to_name=[]
-        with open(args.category_names, 'r') as f:
-            flower_to_name = json.load(f)
-        classes_label=[]
-        print(probs.shape)
-        for i in classes:
-            classes_label.append(flower_to_name.get(str(i)))
-        for i in range(args.top_k):
-            print ("Class name: ",classes_label[i],",Probability :" ,probs[0][i].detach().numpy()*100)
+def train_transformer(train_dir):
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                            transforms.RandomResizedCrop(224),
+                                            transforms.RandomHorizontalFlip(),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                 [0.229, 0.224, 0.225])])
+    train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+    return train_data
+
+def test_transformer(test_dir):
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                           transforms.CenterCrop(224),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                [0.229, 0.224, 0.225])])
+    test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
+    return test_data
+
+def data_loader(data, train=True):
+    if train: 
+        loader = torch.utils.data.DataLoader(data, batch_size=50, shuffle=True)
+    else: 
+        loader = torch.utils.data.DataLoader(data, batch_size=50)
+    return loader
+
+def check_gpu(gpu_arg):
+    if not gpu_arg or gpu_arg.lower() == "cpu":
+        return torch.device("cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if device == "cpu":
+        print("CUDA was not found on device, using CPU instead.")
+    return device
+
+def load_pretrained_model(architecture="vgg16"):
+    model = models.vgg16(pretrained=True)
+    model.name = "vgg16"
+    for param in model.parameters():
+        param.requires_grad = False 
+    return model
+
+def initial_classifier(model, hidden_units):
+    classifier = nn.Sequential(OrderedDict([
+        ('inputs', nn.Linear(25088, hidden_units)),
+        ('relu1', nn.ReLU()),
+        ('dropout', nn.Dropout(0.5)),
+        ('hidden_layer1', nn.Linear(hidden_units, 90)),
+        ('relu2', nn.ReLU()),
+        ('hidden_layer2', nn.Linear(90, 70)),
+        ('relu3', nn.ReLU()),
+        ('hidden_layer3', nn.Linear(70, 102)),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
+    model.classifier = classifier
+    return classifier
+
+def validation(model, testloader, criterion, device):
+    test_loss = 0
+    accuracy = 0
+    model.eval()
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            output = model.forward(inputs)
+            test_loss += criterion(output, labels).item()
+            ps = torch.exp(output)
+            equality = (labels.data == ps.max(dim=1)[1])
+            accuracy += equality.type(torch.FloatTensor).mean()
+    return test_loss, accuracy
+
+def network_trainer(model, trainloader, validloader, device, criterion, optimizer, epochs, print_every):
+    steps = 0
+    model.to(device)
+    for e in range(epochs):
+        running_loss = 0
+        model.train()
+        for ii, (inputs, labels) in enumerate(trainloader):
+            steps += 1
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if steps % print_every == 0:
+                model.eval()
+                with torch.no_grad():
+                    valid_loss, accuracy = validation(model, validloader, criterion, device)
+                print("Epoch: {}/{}.. ".format(e+1, epochs),
+                      "Training Loss: {:.3f}.. ".format(running_loss/print_every),
+                      "Validation Loss: {:.3f}.. ".format(valid_loss/len(validloader)),
+                      "Validation Accuracy: {:.3f}".format(accuracy/len(validloader)))
+                running_loss = 0
+                model.train()
+
+def validate_model(model, testloader, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        model.eval()
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy on test images: {:.2f}%'.format(100 * correct / total))
+
+def save_checkpoint(model, save_dir, train_data):
+    if type(save_dir) == type(None):
+        print("Model checkpoint directory not specified, model will not be saved.")
     else:
-        for i in range(args.top_k):
-            print("Class #", classes[i],"Probability: ",probs[0][i].detach().numpy()*100)
-
+        if isdir(save_dir):
+            model.class_to_idx = train_data.class_to_idx
+            checkpoint = {
+                'architecture': model.name,
+                'classifier': model.classifier,
+                'class_to_idx': model.class_to_idx,
+                'state_dict': model.state_dict()
+            }
+            torch.save(checkpoint, save_dir)
+            print("Model saved successfully!")
+        else: 
+            print("Directory not found, model will not be saved.")
 
 def main():
-    parser = argparse.ArgumentParser(description='Flowers Classifcation Predictor')
-    parser.add_argument('--input', type=str, help='path for image to predict',required=True)
-    parser.add_argument('--checkpoint_path', type=str, default='checkpoint.pth', help='path to en existance  checkpoint',required=True)
-    parser.add_argument('--top_k', type=int, default=5, help='top k classes for the input')
-    parser.add_argument('--category_names', type=str, help='json path file of categories names of flowers')
-    parser.add_argument('--gpu' , type=bool, default=False, help='checkpoint directory path')
-    args = parser.parse_args()
-    print(args)
-    
-    predicts(args)
-    print("\nPrediction is finished\n")
-if __name__ == "__main__":
+    args = arg_parser()
+    data_dir = 'flowers'
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
+    train_data = train_transformer(train_dir)
+    valid_data = test_transformer(valid_dir)
+    test_data = test_transformer(test_dir)
+    trainloader = data_loader(train_data)
+    validloader = data_loader(valid_data, train=False)
+    testloader = data_loader(test_data, train=False)
+    model = load_pretrained_model(architecture=args.arch)
+    model.classifier = initial_classifier(model, hidden_units=args.hidden_units)
+    device = check_gpu(gpu_arg=args.gpu)
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=args.learning_rate)
+    print_every = 30
+    network_trainer(model, trainloader, validloader, device, criterion, optimizer, args.epochs, print_every)
+    print("\nTraining process is completed!")
+    validate_model(model, testloader, device)
+    save_checkpoint(model, args.save_dir, train_data)
+
+if __name__ == '__main__':
     main()
