6de4580a72cadb94dff72f6c0884501896bb2ce7
MADHU <116476425+Madhu-Human-on-Earth@users.noreply.github.com>
2023-10-22 11:28:17 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,84 +1,93 @@
 import argparse
 import json
 import torch
-import numpy as np
-from math import ceil
 from PIL import Image
-from train import check_gpu
+import numpy as np
+from torch import nn
 from torchvision import models
 
-def arg_parser():
-    parser = argparse.ArgumentParser(description="predict.py")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Predict a flower name from an image.")
     parser.add_argument('--image', type=str, help='Path to image file for prediction.', required=True)
     parser.add_argument('--checkpoint', type=str, help='Path to checkpoint file as str.', required=True)
     parser.add_argument('--top_k', type=int, help='Choose top K matches as int.')
     parser.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
     parser.add_argument('--gpu', dest="gpu", action="store_true", default=False)
-
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 def load_checkpoint(checkpoint_path):
     checkpoint = torch.load(checkpoint_path)
-    model = models.vgg16(pretrained=True)
-    model.class_to_idx = checkpoint['class_to_idx']
-    model.classifier = checkpoint['classifier']
+    arch = checkpoint['arch']
+    hidden_units = checkpoint['hidden_units']
+    class_to_idx = checkpoint['class_to_idx']
+    
+    model = getattr(models, arch)(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(25088, hidden_units)),
+        ('relu', nn.ReLU()),
+        ('dropout', nn.Dropout(0.3)),
+        ('fc2', nn.Linear(hidden_units, len(class_to_idx))),
+        ('output', nn.LogSoftmax(dim=1))
+    ]))
+    
+    model.classifier = classifier
     model.load_state_dict(checkpoint['state_dict'])
+    model.class_to_idx = class_to_idx
+    
     return model
 
 def process_image(image_path):
-    img = Image.open(image_path)
-    size = 256, 256
-    img.thumbnail(size)
-    width, height = img.size
-    crop_size = 244
-    left = (width - crop_size) / 2
-    top = (height - crop_size) / 2
-    right = (width + crop_size) / 2
-    bottom = (height + crop_size) / 2
-    img = img.crop((left, top, right, bottom))
-    np_image = np.array(img) / 255.0
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    np_image = (np_image - mean) / std
-    np_image = np_image.transpose((2, 0, 1))
-    return np_image
+    image = Image.open(image_path)
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    image_tensor = transform(image)
+    return image_tensor
 
-def predict(image_tensor, model, device, cat_to_name, topk=5):
+def predict(image_path, model, topk, device):
     model.eval()
+    image_tensor = process_image(image_path)
+    image_tensor = image_tensor.unsqueeze(0)
+    image_tensor = image_tensor.to(device)
+    
     with torch.no_grad():
-        image_tensor = torch.from_numpy(image_tensor).float()
-        image_tensor = image_tensor.unsqueeze(0)
-        image_tensor = image_tensor.to(device)
-        output = model(image_tensor)
-        ps = torch.exp(output)
-        top_probs, top_indices = ps.topk(topk, dim=1)
-        top_probs = top_probs.cpu().numpy().tolist()[0]
-        top_indices = top_indices.cpu().numpy().tolist()[0]
-        idx_to_class = {val: key for key, val in model.class_to_idx.items()}
-        top_labels = [idx_to_class[idx] for idx in top_indices]
-        top_flowers = [cat_to_name[label] for label in top_labels]
-        return top_probs, top_labels, top_flowers
+        output = model.forward(image_tensor)
+        probabilities, indices = torch.topk(torch.exp(output), topk)
+        probabilities = probabilities.squeeze().tolist()
+        indices = indices.squeeze().tolist()
+    
+    return probabilities, indices
 
-def print_probability(probs, flowers):
-    for i, (flower, prob) in enumerate(zip(flowers, probs), 1):
-        print(f"Rank {i}: Flower: {flower}, Likelihood: {ceil(prob * 100)}%")
+def load_category_names(category_names):
+    with open(category_names, 'r') as f:
+        cat_to_name = json.load(f)
+    return cat_to_name
 
 def main():
-    args = arg_parser()
+    args = parse_args()
     
-    with open(args.category_names, 'r') as f:
-        cat_to_name = json.load(f)
+    device = torch.device("cuda" if args.gpu and torch.cuda.is_available() else "cpu")
     
     model = load_checkpoint(args.checkpoint)
+    model.to(device)
+    model.eval()
     
-    image_tensor = process_image(args.image)
+    image_path = args.image
+    topk = args.top_k if args.top_k else 1
+    cat_to_name = load_category_names(args.category_names)
     
-    device = check_gpu(args.gpu)
+    probabilities, indices = predict(image_path, model, topk, device)
+    flower_names = [cat_to_name[str(index)] for index in indices]
     
-    top_probs, top_labels, top_flowers = predict(image_tensor, model, device, cat_to_name, args.top_k)
-    
-    print_probability(top_probs, top_flowers)
+    print(f"Top {topk} flower prediction:")
+    for flower, probability in zip(flower_names, probabilities):
+        print(f"Flower: {flower}, Probability: {probability:.4f}")
 
 if __name__ == '__main__':
     main()
